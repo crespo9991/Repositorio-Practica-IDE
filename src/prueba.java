f1e0531f5cccc63c0509cc818afb17cbdf6d89cf
Juan José Crespo Márquez <91crespo99@gmail.com>
2020-01-30 09:56:21 +0100

    Prueba 1.0

---
+++ src/prueba.java
@@ -0,0 +1,12 @@
+
+public class prueba {
+
+	public static void main(String[] args) {
+		String nombre= "Juan José";
+		String apellido1 = "Crespo";
+		String apellido2 ="Márquez";
+		System.out.println("Mi nombre es "+ nombre+" "+ apellido1+" "+ apellido2);
+
+	}
+
+}
